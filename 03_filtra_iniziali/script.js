f6d464dc47b538a007d5a40e60c4fa233080ecc5
ElvisBinaku <elvisbinaku@icloud.com>
2025-12-17 22:47:03 +0100

    creata funzione che ricerca tra un arrey i nomi che inziano con una lettera data dall'utente

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function cercaNomi (nomi, lettera) {
+        let nomiTrovati = [];
+    for (let index = 0; index < nomi.length; index++){
+      
+       
+       if(nomi[index].charAt(0) === lettera){
+        nomiTrovati.push(nomi[index])
+       }
+    
+    }
+    return nomiTrovati
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(cercaNomi(names, prompt("inserisci una lettera")))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
