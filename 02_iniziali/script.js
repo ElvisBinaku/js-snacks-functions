db32a2c0f2c0096695f994b39c518dd352c3d0fd
ElvisBinaku <elvisbinaku@icloud.com>
2025-12-17 22:13:35 +0100

    creata una funzione che ende da un array le inziali e le salva su una variabile e la ritorna

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function firstLetter (parola){
+    let letters = [];
+    for (let index = 0; index < parola.length; index++) {   
+        letters.push(parola[index].charAt(0))   
+    }
+        return letters
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
